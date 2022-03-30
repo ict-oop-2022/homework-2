2287c6fe757b45f794abec8e9a1a5ee92813c1a3
Nath-han <nathanbounkouta@gmail.com>
2022-03-30 17:39:57 +0300

    Added number

--- number/number.h
+++ number/number.h
@@ -4,5 +4,62 @@
 #include <iostream>
 
 class number_t {
-
+public:
+    number_t();
+    number_t(const number_t&);
+    number_t(int64_t);
+    number_t(int);
+    
+    explicit operator bool();
+    explicit operator int64_t();
+    explicit operator int();
+    explicit operator std::string();
+    
+    friend number_t operator+(const number_t &lhs, const number_t& rhs);
+    friend number_t operator-(const number_t &lhs, const number_t& rhs);
+    friend number_t operator*(const number_t &lhs, const number_t& rhs);
+    friend number_t operator/(const number_t &lhs, const number_t& rhs);
+    friend number_t operator%(const number_t &lhs, const number_t& rhs);
+    
+    friend number_t operator&(const number_t &lhs, const number_t& rhs);
+    friend number_t operator^(const number_t &lhs, const number_t& rhs);
+    friend number_t operator|(const number_t &lhs, const number_t& rhs);
+    
+    friend number_t operator<<(const number_t &lhs, const number_t& rhs);
+    friend number_t operator>>(const number_t &lhs, const number_t& rhs);
+    
+    number_t& operator+=(const number_t &other);
+    number_t& operator-=(const number_t &other);
+    number_t& operator*=(const number_t &other);
+    number_t& operator/=(const number_t &other);
+    number_t& operator%=(const number_t &other);
+    
+    number_t& operator&=(const number_t &other);
+    number_t& operator^=(const number_t &other);
+    number_t& operator|=(const number_t &other);
+    
+    number_t& operator<<=(const number_t &other);
+    number_t& operator>>=(const number_t &other);
+    
+    number_t operator-() const;
+    number_t operator+() const;
+    number_t operator~() const;
+    number_t operator!() const;
+    
+    number_t& operator++();
+    number_t operator++(int);
+    number_t& operator--();
+    number_t operator--(int);
+    
+    friend bool operator==(const number_t &lhs, const number_t &rhs);
+    friend bool operator!=(const number_t &lhs, const number_t &rhs);
+    friend bool operator<(const number_t &lhs, const number_t &rhs);
+    friend bool operator>(const number_t &lhs, const number_t &rhs);
+    friend bool operator<=(const number_t &lhs, const number_t &rhs);
+    friend bool operator>=(const number_t &lhs, const number_t &rhs);
+    
+    friend std::istream& operator>>(std::istream&, number_t&);
+    friend std::ostream& operator<<(std::ostream&, const number_t&);
+private:
+    int64_t inner;
 };
