18ae2131bb3c5e1a32e28b43468ee0878f98ccbe
Sveta-ops <71131655+Sveta-ops@users.noreply.github.com>
2022-04-19 21:23:22 +0300

    Update trie.h

--- trie/trie.h
+++ trie/trie.h
@@ -2,71 +2,34 @@
 
 #include <string>
 #include <vector>
-
 #include <map>
+#include <memory>
+
+struct leaves;
 
 class trie_t {
+
+    std::shared_ptr<leaves> root;
+
 public:
-  trie_t();
-  ~trie_t();
+    trie_t();
+    trie_t(const trie_t &trie);
 
-  trie_t(const trie_t &other);
-  trie_t &operator=(const trie_t &other);
+    ~trie_t();
 
-  /**
-   * Insert specified string
-   * @param str
-   */
-  void insert(const std::string &str);
-  /**
-   * Remove specified string
-   * @param str
-   * @return return true if specified string was in container
-   */
-  bool erase(const std::string &str);
-  /**
-   * Made your container empty
-   */
-  void clear();
+    trie_t &operator=(const trie_t &trie);
+    std::string operator[](size_t index) const;
 
-  /**
-   * @param str
-   * @return true if there is at least one specified string in your container
-   */
-  bool find(const std::string &str) const;
-  /**
-   * @param prefix
-   * @return number of strings which starts with specified prefix in your container
-   */
-  size_t count_with_prefix(const std::string &prefix) const;
+    void insert(const std::string &s);
+    void swap(trie_t &trie);
+    void clear();
 
-  /**
-   * IMAGINE that you sort all strings in you container in lexicographic order.
-   * You should return string on position index in this order
-   * @param index
-   */
-  std::string operator[](size_t index) const;
+    bool erase(const std::string &s);
+    bool find(const std::string &s) const;
+    bool empty() const;
 
-  /**
-   * @return All strings from your container in lexicographic order.
-   */
-  std::vector<std::string> to_vector() const;
+    size_t count_with_prefix(const std::string &prefix) const;
+    size_t size() const;
 
-  /**
-   * @return Number of strings in container
-   */
-  size_t size() const;
-  /**
-   * @return true if container is empty :)
-   */
-  bool empty() const;
-
-  /**
-   * Exchange content of this and other
-   * @param other
-   */
-  void swap(trie_t &other);
-
-private:
-
+    std::vector<std::string> to_vector() const;
 };
