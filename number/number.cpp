f2851facb51f92075fc13ff31c6c7baee35ea4e4
Ира Крамаренко <70940678+IrinaKramarenko@users.noreply.github.com>
2022-06-29 22:02:30 +0300

    Add files via upload

--- number/number.cpp
+++ number/number.cpp
@@ -1,1 +1,188 @@
 #include "number.h"
+
+number_t::number_t() {
+  _data = 0;
+}
+
+number_t::number_t(const number_t& other) {
+  _data = other._data;
+}
+
+number_t::number_t(int64_t data) {
+  _data = data;
+}
+
+number_t& number_t::operator=(const number_t& other) {
+  _data = other._data;
+  return *this;
+}
+
+
+number_t::operator int64_t() const {
+  return _data;
+}
+
+number_t::operator bool() const {
+  return _data != 0;
+}
+
+number_t::operator std::string() const {
+  return std::to_string(_data);
+}
+
+number_t operator+(const number_t& first, const number_t& second) {
+  return number_t{ first._data + second._data };
+}
+
+number_t operator-(const number_t& first, const number_t& second) {
+  return number_t{ first._data - second._data };
+}
+
+number_t operator/(const number_t& first, const number_t& second) {
+  return number_t{ first._data / second._data };
+}
+
+number_t operator*(const number_t& first, const number_t& second) {
+  return number_t{ first._data * second._data };
+}
+
+number_t operator%(const number_t& first, const number_t& second) {
+  return number_t{ first._data % second._data };
+}
+
+number_t operator|(const number_t& first, const number_t& second) {
+  return number_t{ first._data | second._data };
+}
+
+number_t operator&(const number_t& first, const number_t& second) {
+  return number_t{ first._data & second._data };
+}
+
+number_t operator^(const number_t& first, const number_t& second) {
+  return number_t{ first._data ^ second._data };
+}
+
+number_t operator>>(const number_t& first, uint8_t second) {
+  return number_t{ first._data >> second };
+}
+
+number_t operator<<(const number_t& first, uint8_t second) {
+  return number_t{ first._data << second };
+}
+
+number_t& number_t::operator+=(const number_t& other) {
+  _data += other._data;
+  return *this;
+}
+
+number_t& number_t::operator-=(const number_t& other) {
+  _data -= other._data;
+  return *this;
+}
+
+number_t& number_t::operator*=(const number_t& other) {
+  _data *= other._data;
+  return *this;
+}
+
+number_t& number_t::operator/=(const number_t& other) {
+  _data /= other._data;
+  return *this;
+}
+
+number_t& number_t::operator%=(const number_t& other) {
+  _data %= other._data;
+  return *this;
+}
+
+number_t& number_t::operator|=(const number_t& other) {
+  _data |= other._data;
+  return *this;
+}
+
+number_t& number_t::operator&=(const number_t& other) {
+  _data &= other._data;
+  return *this;
+}
+
+number_t& number_t::operator^=(const number_t& other) {
+  _data ^= other._data;
+  return *this;
+}
+
+number_t& number_t::operator>>=(uint8_t shift) {
+  _data >>= shift;
+  return *this;
+}
+
+number_t& number_t::operator<<=(uint8_t shift) {
+  _data <<= shift;
+  return *this;
+}
+
+number_t number_t::operator+() const {
+  return number_t{ +_data };
+}
+number_t number_t::operator-() const {
+  return number_t{ -_data };
+}
+number_t number_t::operator~() const {
+  return number_t{ ~_data };
+}
+bool number_t::operator!() const {
+  return !this->operator bool();
+}
+
+number_t number_t::operator++(int) {
+  int64_t temp = _data;
+  ++_data;
+  return number_t(temp);
+}
+
+number_t& number_t::operator++() {
+  ++_data;
+  return *this;
+}
+
+number_t number_t::operator--(int) {
+  int64_t temp = _data;
+  --_data;
+  return number_t(temp);
+}
+
+number_t& number_t::operator--() {
+  --_data;
+  return *this;
+}
+
+bool operator==(const number_t& first, const number_t& second) {
+  return first._data == second._data;
+}
+
+bool operator!=(const number_t& first, const number_t& second) {
+  return first._data != second._data;
+}
+
+bool operator<=(const number_t& first, const number_t& second) {
+  return first._data <= second._data;
+}
+
+bool operator>=(const number_t& first, const number_t& second) {
+  return first._data >= second._data;
+}
+
+bool operator<(const number_t& first, const number_t& second) {
+  return first._data < second._data;
+}
+
+bool operator>(const number_t& first, const number_t& second) {
+  return first._data > second._data;
+}
+
+std::istream& operator>>(std::istream& in, number_t& number) {
+  return in >> number._data;
+}
+
+std::ostream& operator<<(std::ostream& out, const number_t& number) {
+  return out << number._data;
+}
--- number/number.h
+++ number/number.h
@@ -2,7 +2,65 @@
 
 #include <cstdint>
 #include <iostream>
+#include <string>
 
 class number_t {
+  int64_t _data{0};
+public:
+  number_t();
+  number_t(const number_t& other);
+  number_t(int64_t value);
 
+  number_t& operator=(const number_t& other);
+
+  explicit operator bool() const;
+  explicit operator int64_t() const;
+  explicit operator std::string() const;
+
+  friend number_t operator+(const number_t& first, const number_t& second);
+  friend number_t operator-(const number_t& first, const number_t& second);
+  friend number_t operator/(const number_t& first, const number_t& second);
+  friend number_t operator*(const number_t& first, const number_t& second);
+  friend number_t operator%(const number_t& first, const number_t& second);
+
+  friend number_t operator|(const number_t& first, const number_t& second);
+  friend number_t operator&(const number_t& first, const number_t& second);
+  friend number_t operator^(const number_t& first, const number_t& second);
+
+  friend number_t operator>>(const number_t& number, uint8_t shift);
+  friend number_t operator<<(const number_t& number, uint8_t shift);
+
+  number_t& operator+=(const number_t& other);
+  number_t& operator-=(const number_t& other);
+  number_t& operator*=(const number_t& other);
+  number_t& operator/=(const number_t& other);
+  number_t& operator%=(const number_t& other);
+
+  number_t& operator|=(const number_t& other);
+  number_t& operator&=(const number_t& other);
+  number_t& operator^=(const number_t& other);
+
+  number_t& operator>>=(uint8_t shift);
+  number_t& operator<<=(uint8_t shift);
+
+  number_t operator+() const;
+  number_t operator-() const;
+  number_t operator~() const;
+  bool operator!() const;
+
+  number_t operator++(int);
+  number_t& operator++();
+
+  number_t operator--(int);
+  number_t& operator--();
+
+  friend bool operator==(const number_t& first, const number_t& second);
+  friend bool operator!=(const number_t& first, const number_t& second);
+  friend bool operator<=(const number_t& first, const number_t& second);
+  friend bool operator>=(const number_t& first, const number_t& second);
+  friend bool operator<(const number_t& first, const number_t& second);
+  friend bool operator>(const number_t& first, const number_t& second);
+
+  friend std::istream& operator>>(std::istream& in, number_t& number);
+  friend std::ostream& operator<<(std::ostream& out, const number_t& number);
 };
