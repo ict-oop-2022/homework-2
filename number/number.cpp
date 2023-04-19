ab97cfdba08946d41c40466734b7035281dfe370
SnooPyn <bellazreganis09@gmail.com>
2023-04-19 17:47:26 +0300

    homework2

--- number/number.cpp
+++ number/number.cpp
@@ -1,1 +1,96 @@
 #include "number.h"
+
+// Default constructor and destructor
+number_t::number_t() = default;
+number_t::~number_t() = default;
+
+// Constructor with an int64_t argument
+number_t::number_t(const int64_t a) : val(a) {}
+
+// Copy constructor
+number_t::number_t(const number_t &a) : val(a.val) {}
+
+// Assignment operator
+number_t &number_t::operator=(const number_t &a) {
+  val = a.val;
+  return *this;
+}
+
+// Arithmetic assignment operators
+number_t &number_t::operator+=(const number_t &a) {
+  val += a.val;
+  return *this;
+}
+number_t &number_t::operator-=(const number_t &a) {
+  val -= a.val;
+  return *this;
+}
+number_t &number_t::operator*=(const number_t &a) {
+  val *= a.val;
+  return *this;
+}
+number_t &number_t::operator/=(const number_t &a) {
+  val /= a.val;
+  return *this;
+}
+number_t &number_t::operator%=(const number_t &a) {
+  val %= a.val;
+  return *this;
+}
+number_t &number_t::operator^=(const number_t &a) {
+  val ^= a.val;
+  return *this;
+}
+number_t &number_t::operator&=(const number_t &a) {
+  val &= a.val;
+  return *this;
+}
+number_t &number_t::operator|=(const number_t &a) {
+  val |= a.val;
+  return *this;
+}
+number_t &number_t::operator<<=(const number_t &a) {
+  val <<= a.val;
+  return *this;
+}
+number_t &number_t::operator>>=(const number_t &a) {
+  val >>= a.val;
+  return *this;
+}
+
+// Increment and decrement operators
+number_t &number_t::operator++() {
+  ++val;
+  return *this;
+}
+number_t &number_t::operator--() {
+  --val;
+  return *this;
+}
+number_t number_t::operator++(int) {
+  return number_t(val++);
+}
+number_t number_t::operator--(int) {
+  return number_t(val--);
+}
+
+// Conversion operators
+number_t::operator bool() const {
+  return val != 0;
+}
+number_t::operator int64_t() const {
+  return val;
+}
+number_t::operator std::string() const {
+  return std::to_string(val);
+}
+
+// Input/output stream operators
+std::ostream &operator<<(std::ostream &os, const number_t &a) {
+  os << a.val;
+  return os;
+}
+std::istream &operator>>(std::istream &is, number_t &a) {
+  is >> a.val;
+  return is;
+}
--- number/number.h
+++ number/number.h
@@ -2,7 +2,39 @@
 
 #include <cstdint>
 #include <iostream>
+#include <string>
 
 class number_t {
+public:
+number_t() : val(0) {} // Default constructor
+~number_t() {} // Destructor
+number_t(const int64_t num) : val(num) {} // Constructor from a number
+number_t(const number_t &num) : val(num.val) {} // Copy constructor
 
+explicit operator bool() const { return val != 0; }
+operator int64_t() const { return val; } // Cast to number
+operator std::string() const { return std::to_string(val); } // Cast to string
+
+number_t& operator=(const number_t &rhs) { val = rhs.val; return *this; } // Assignment operator
+number_t& operator+=(const number_t &rhs) { val += rhs.val; return *this; } // Addition assignment operator
+number_t& operator-=(const number_t &rhs) { val -= rhs.val; return *this; } // Subtraction assignment operator
+number_t& operator*=(const number_t &rhs) { val *= rhs.val; return *this; } // Multiplication assignment operator
+number_t& operator/=(const number_t &rhs) { val /= rhs.val; return *this; } // Division assignment operator
+number_t& operator%=(const number_t &rhs) { val %= rhs.val; return *this; } // Modulo assignment operator
+number_t& operator^=(const number_t &rhs) { val ^= rhs.val; return *this; } // Bitwise XOR assignment operator
+number_t& operator&=(const number_t &rhs) { val &= rhs.val; return *this; } // Bitwise AND assignment operator
+number_t& operator|=(const number_t &rhs) { val |= rhs.val; return *this; } // Bitwise OR assignment operator
+number_t& operator<<=(const number_t &rhs) { val <<= rhs.val; return *this; } // Left shift assignment operator
+number_t& operator>>=(const number_t &rhs) { val >>= rhs.val; return *this; } // Right shift assignment operator
+
+number_t& operator++() { ++val; return *this; } // Prefix increment operator
+number_t& operator--() { --val; return *this; } // Prefix decrement operator
+number_t operator++(int) { number_t temp(*this); ++val; return temp; } // Postfix increment operator
+number_t operator--(int) { number_t temp(*this); --val; return temp; } // Postfix decrement operator
+
+friend std::ostream &operator<<(std::ostream &out, const number_t &num) { out << num.val; return out; } // Output operator
+friend std::istream &operator>>(std::istream &in, number_t &num) { in >> num.val; return in; } // Input operator
+
+private:
+int64_t val; // Numerical value
 };
